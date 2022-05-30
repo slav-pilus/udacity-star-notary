22664e1c703dfd4314bbfdd94fd7f8218ca9ab11
Slav Pilus <slav.pilus@envoydigital.com>
2022-05-30 09:00:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Specify the Truffle version and OpenZeppelin version used in the project.
+Token Name - Universal Star Register Token
+Token Symbol - USRT
+Your “Token Address” on the Rinkeby Network - 0xAcD25F9c792B2d41578577843934c5D459547B51
+
+https://rinkeby.etherscan.io/token/0xAcD25F9c792B2d41578577843934c5D459547B51
---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "5777",    // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider (mnemonic, `https://rinkeby.infura.io/v3/3e15e0e35cf24f869740d415012512b7`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+}
